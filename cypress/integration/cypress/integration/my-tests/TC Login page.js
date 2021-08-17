aa28414f1a76dadc02af962af2f9c4bda362a2ed
ElenaCocariu <elenacocariu@gmail.com>
2021-08-17 17:29:08 +0300

    Modificari teste3

--- cypress/integration/cypress/integration/my-tests/TC Login page.js
+++ cypress/integration/cypress/integration/my-tests/TC Login page.js
@@ -12,6 +12,8 @@ describe('Check the "Login" Page', () => {
     cy.get('#email').type('mihaelabarba@gmail.com').should('have.value','mihaelabarba@gmail.com' )
     cy.get('#pass').type('123456789').should('have.value','123456789')
     cy.get('#send2 > :nth-child(1) > span').click()
+    cy.get('body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div > div.page-title > h1').should('have.text', 'My Dashboard')
+
    
    })
 
@@ -25,16 +27,37 @@ describe('Check the "Login" Page', () => {
     cy.get('#advice-required-entry-pass').should('have.text','This is a required field.')
    })
 
-
-   it('Welcome "My Dashboard"', () => {
+  it('Customer login with empty email', () => {
     cy.get('body div.header-language-background p')
     cy.get('.skip-account').click()
     cy.get('#header-account > .links > ul > .last > a').click()
-    cy.get('#email').type('mihaelabarba@gmail.com').should('have.value','mihaelabarba@gmail.com' )
-    cy.get('#pass').type('123456789').should('have.value','123456789')
+    cy.get('#email')
+    cy.get('#pass').type('123456789')
     cy.get('#send2 > :nth-child(1) > span').click()
-    cy.get('body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div > div.page-title > h1').should('have.text','My Dashboard')
-       
+    cy.get('#advice-required-entry-pass').should('have.text','This is a required field.')
+   })
+
+
+   it('Customer login with wrong email', () => {
+    cy.get('body div.header-language-background p')
+    cy.get('.skip-account').click()
+    cy.get('#header-account > .links > ul > .last > a').click()
+    cy.get('#email').type('mihaelabarba@gmail.rooo').should('have.value','mihaelabarba@gmail.rooo' )
+    cy.get('#pass').type('123456789')
+    cy.get('#send2 > :nth-child(1) > span').click()
+    cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','Invalid login or password.')
+    
+   })
+
+   it('Customer login with wrong password', () => {
+    cy.get('body div.header-language-background p')
+    cy.get('.skip-account').click()
+    cy.get('#header-account > .links > ul > .last > a').click()
+    cy.get('#email').type('mihaelabarba@gmail.com').should('have.value','mihaelabarba@gmail.com')
+    cy.get('#pass').type('123456789hdfhdhgfj').should('have.value','123456789hdfhdhgfj')
+    cy.get('#send2 > :nth-child(1) > span').click()
+    cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','Invalid login or password.')
+    
    })
 
 
--- cypress/integration/cypress/integration/my-tests/Valid Login.js
+++ cypress/integration/cypress/integration/my-tests/Valid Login.js
@@ -12,6 +12,7 @@ describe('Validation of the "Login" Page', () => {
       cy.get('#email').type('mihaelabarba@gmail.com').should('have.value','mihaelabarba@gmail.com' )
       cy.get('#pass').type('123456789').should('have.value','123456789')
       cy.get('#send2 > :nth-child(1) > span').click()
+      cy.get('body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div > div.page-title > h1').should('have.text', 'My Dashboard')
      
      })
     
